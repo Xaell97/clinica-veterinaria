57c703ad1bd520831822d6348f733287aca129d5
Michael Jobson da Silva <man0048256@aluno.firjansenaisesi.com.br>
2026-08-12 18:27:14 -0300

    Feature(auth): Cria a autenticação de usuários do sistema

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,32 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+/**
+ * Rotas do Dashboard
+ */
+Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])
+        ->name('dashboard');
+});
+
+
+
+
+/**
+ * Rotas de Login
+ */
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+
+Route::post('/login', [AuthController::calss, 'login']);
+
+Route::post('/logout', [AuthController::class, 'logout'])
+    ->middleware('auth')
+    ->name('logout');
